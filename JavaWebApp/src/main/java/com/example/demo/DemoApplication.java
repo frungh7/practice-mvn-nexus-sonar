2068fd641fbeee0a5338294253ca63a7dc7726f3
unknown <frunche1@gmail.com>
2022-10-18 21:11:57 -0500

    modified bug-fix-123

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name ="Ghislaine"
+		string name ="Ghislaine";
 
-		System.out.println(name)
+		System.out.println(name);
+
+		interger age ="20";
+
+		System.out.println(age + 5)	;
 	}
-
-}
