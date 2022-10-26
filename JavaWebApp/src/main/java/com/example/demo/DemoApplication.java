97d0070b98fc4b203b8ea3521750ec93811913b6
unknown <frunche1@gmail.com>
2022-10-25 21:54:29 -0500

    main-fix2

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,10 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		  String name = "Ghislaine";
-
-		System.out.println(name);
-	}
-
-}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "ghislaine";
+        System.out.println(name);
+    }
