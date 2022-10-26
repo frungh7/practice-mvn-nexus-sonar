15695e744d4a1861de1f6e23867a3235de1a90a0
unknown <frunche1@gmail.com>
2022-10-25 20:15:10 -0500

    main-fix2

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name ="Ghislaine"
+		string name ="Ghislaine";
 
-		System.out.println(name)
+		System.out.println(name);
 	}
 
 }
